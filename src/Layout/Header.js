2a0332a3135f2d28c63f3fb764f66a6dbda10e1b
Davids_Code_Life <dwright9157@icloud.com>
2023-10-11 14:14:46 -0500

    Tear-Down Update
    Rebuilt Website

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 function Header() {
-    return (
-        <header className="jumbotron bg-dark">
-            <div className="container text-white">
-                <h1 className="display-4">Flash Card-O-Matic </h1>
-                <p className="lead">Created with React</p>
-            </div>
-        </header>
-    );
+  return (
+    <header className="jumbotron bg-dark">
+      <div className="container text-white">
+        <h1 className="display-4">Flash Card-O-Matic</h1>
+        <p className="lead">Created With React</p>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
