be1dacef98012eb82e3ae73f4a8207e419da19d9
Davids_Code_Life <dwright9157@icloud.com>
2023-10-16 12:41:03 -0500

    Major Updates Patch V2.03

--- src/Layout/Home.js
+++ src/Layout/Home.js
@@ -2,10 +2,18 @@ import React from "react";
 import Card from "./Card";
 
 const Home = ({ decks, deleteDeckHandler }) => {
-    const deckList = decks.map((e) => (
-        <Card flashcard={e} deleteDeckHandler={deleteDeckHandler} key={e.id} />
-    ));
-    return <div>{decks && deckList}</div>;
+  // Sort the decks array to ensure the "Tutorial" deck appears first
+  const sortedDecks = [...decks].sort((a, b) => {
+    if (a.name === "Tutorial") return -1;
+    if (b.name === "Tutorial") return 1;
+    return 0; // No change in order for other decks
+  });
+
+  const deckList = sortedDecks.map((e) => (
+    <Card flashcard={e} deleteDeckHandler={deleteDeckHandler} key={e.id} />
+  ));
+
+  return <div>{deckList}</div>;
 };
 
 export default Home;
