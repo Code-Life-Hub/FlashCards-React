53414f03e804c3eb5b4fc5926205ca5945142653
Davids_Code_Life <dwright9157@icloud.com>
2023-10-16 09:54:34 -0500

    Update to Header.js

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -4,8 +4,8 @@ function Header() {
   return (
     <header className="jumbotron bg-dark">
       <div className="container text-white">
-        <h1 className="display-4">Flash Card-O-Matic</h1>
-        <p className="lead">Created With React</p>
+        <h1 className="display-4">Flash Cards</h1>
+        <p className="lead">Developed with React | JavaScript | HTML5 | CSS </p>
       </div>
     </header>
   );
